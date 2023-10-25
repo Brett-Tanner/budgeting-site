32f536124c3d244293e3cd51d72611675476ba79
Brett Tanner <bretttanner171@gmail.com>
2023-10-25 23:18:02 +0900

    Initial VercelPG/Drizzle setup

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="astro/client" />
+
+export declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      POSTGRES_DATABASE: string;
+      POSTGRES_HOST: string;
+      POSTGRES_PASSWORD: string;
+      POSTGRES_PRISMA_URL: string;
+      POSTGRES_URL: string;
+      POSTGRES_USER: string;
+      VERCEL: string;
+      VERCEL_ENV: string;
+    }
+  }
+}
